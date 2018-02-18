cde9bdb656d56c574ca4386ad6b56f1bd2a7fbb8
Sebastian M Zdroana <s.zdroana@gmail.com>
2018-02-18 14:51:33 +0200

    stack class being called in main for the first time, for testing purposes, please ignore

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
-
+//404 = stack empty 504 = stack full
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Stack stack = new Stack(10);
+
+        stack.printStack();
+
+        stack.push(3);
+        stack.push(4);
+        stack.push(1);
+
+        stack.printStack();
+
+        if (stack.pop() == -404){
+            System.out.println("Error, stack is already empty");
+        }
+
+        stack.push(9);
+
+        stack.printStack();
+
     }
 }
